3c9eef5919f490af903fc74621c29f5adeb8eabf
REGIVAN33 <rregivan.c.s@gmail.com>
2025-08-19 21:37:21 -0300

    Aula

--- calculadora_imc.py
+++ calculadora_imc.py
@@ -5,10 +5,16 @@ peso = float(input("Agora digite seu peso: "))
 altura = float(input("Por fim, digite sua altura: "))
 seu_imc = peso / (altura * 2)
 
-if seu_imc <= 18.5 :
+if seu_imc < 18.5 :
     print(f"{nome}, seu IMC {seu_imc:.2f} está abaixo do peso")
 elif seu_imc <= 24.9 :
     print(f"{nome}, seu IMC {seu_imc:.2f} esta normal")
-else :
-    print(f"{nome}, seu IMC {seu_imc:.2f} está acima do peso")
+elif seu_imc <= 30:
+    print(f"{nome}, seu IMC {seu_imc:.2f} sobrepeso")
+elif seu_imc <= 35:
+    print(f"{nome}, seu IMC {seu_imc:.2f} Obesidade grau I")
+elif seu_imc <= 40:
+    print(f"{nome}, seu IMC {seu_imc:.2f} Obesidade grau II")
+else:
+    print(f"{nome}, seu IMC {seu_imc:.2f} Obesidade grau III (Mórbida)")
     
