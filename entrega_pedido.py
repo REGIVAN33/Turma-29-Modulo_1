80cef098f3f518d037ff17434e0e33548b81cfe5
REGIVAN33 <rregivan.c.s@gmail.com>
2025-08-21 21:33:07 -0300

    commit

--- entrega_pedido.py
+++ entrega_pedido.py
@@ -1,10 +1,12 @@
 # entrega de pedido
 
+valor_total = 50.00
+area_entrega = "Centro"
 valor_total = float(input("Digite o valor total do pedido: "))
 area_entrega = input("Digite a área de entrega: ")
 
 
-if valor_total >= 50.00 and area_entrega == "Centro":
+if valor_total >= 50.00 and (area_entrega == "Centro" or area_entrega == "Centenário"):
     print("Seu pedido será entregue!")
 else:
     print("Desculpe, seu pedido não cumpre os requisitos de entrega")
